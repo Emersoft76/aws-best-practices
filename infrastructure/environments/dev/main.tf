dd71bf8470ad1add2ca93c509e9ee4ca058fbcfd
Emerson Maciel <59801743+Emersoft76@users.noreply.github.com>
2025-04-04 11:52:22 +0100

    main.tf

---
+++ infrastructure/environments/dev/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source          = "../../modules/vpc"
+  vpc_cidr        = var.vpc_cidr
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+  region          = var.region
+}
